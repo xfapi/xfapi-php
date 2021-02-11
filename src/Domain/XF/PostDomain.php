6a65b3b3ab58e7b84aaf80f19d5f8458c248a759
MattRogowski <matt@rogow.ski>
2021-02-11 16:57:47 +0000

    Creating posts

--- src/Domain/XF/PostDomain.php
+++ src/Domain/XF/PostDomain.php
@@ -8,12 +8,33 @@ use XFApi\Dto\XF\PostsDto;
 
 class PostDomain extends AbstractDomain
 {
+    public function create($threadId, $message, array $data = [])
+    {
+        $uri = $this->getUri();
+        $post = $this->post($uri, [], array_merge([
+            'thread_id' => $threadId,
+            'message' => $message,
+        ], $data));
+
+        return $this->getDto(PostDto::class, $post['post']);
+    }
+
     protected function getUri($uri = null, array $params = [])
     {
+        $return = 'posts';
+        if (isset($params['post_id'])) {
+            $return .= '/' . $params['post_id'];
+        }
+
+        if (!empty($uri)) {
+            $return .= '/' . $uri;
+        }
+
+        return $return;
     }
 
     protected function getDtoClass()
     {
-        return ThreadDto::class;
+        return PostDto::class;
     }
 }
